afb94b9bb95692f44a2528f866df276038f79729
Haritha Tennakoon <haritha.tennakoon@protendering.com>
2022-02-12 14:43:47 +0530

    Task-04: Used third party tool Agility pack to download the html string and manipulate the string.

--- Tretton37/Tretton37/Program.cs
+++ Tretton37/Tretton37/Program.cs
@@ -1,17 +1,28 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using Tretton37.Core;
 using Tretton37.Helpers;
 
 namespace Tretton37
 {
-    class Program
+    internal class Program
     {
         private static readonly LogHelper logHelper = new LogHelper();
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             logHelper.Write(Constants.LogMessages.StartingProgram);
             string uri = new ArgumentHelper().GetWebsiteLink(args);
+
+            // uri is never gonna empty, so we don't need to handle for null.
+            try
+            {
+                var document = new HtmlWeb().Load(uri);
+            }
+            catch (Exception ex)
+            {
+                logHelper.Write(ex.Message);
+            }
         }
     }
 }
