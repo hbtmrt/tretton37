dada43a6b033d3507fbdcb52a6cd3453d62ac28d
Haritha Tennakoon <haritha.tennakoon@protendering.com>
2022-02-12 08:23:05 +0530

    task-01: Added basic file structure

---
+++ Tretton37/Tretton37/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tretton37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
